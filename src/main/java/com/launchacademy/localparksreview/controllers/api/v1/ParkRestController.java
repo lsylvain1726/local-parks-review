a47b3a7880a125eb65f94426b5e395d3cd41f675
Aslihan <aslihanasadova@gmail.com>
2020-05-06 15:12:27 -0400

    Merge branch 'master' of https://github.com/lsylvain1726/local-parks-review into addAForm
    # Conflicts:
    #	src/main/frontend/app/components/ParkList.js
    #	src/main/frontend/app/main.js

--- src/main/java/com/launchacademy/localparksreview/controllers/api/v1/ParkRestController.java
+++ src/main/java/com/launchacademy/localparksreview/controllers/api/v1/ParkRestController.java
@@ -2,17 +2,28 @@ package com.launchacademy.localparksreview.controllers.api.v1;
 
 import com.launchacademy.localparksreview.exceptions.UrlNotFoundException;
 import com.launchacademy.localparksreview.models.Park;
+import com.launchacademy.localparksreview.models.Visitor;
 import com.launchacademy.localparksreview.repositories.ParkRepository;
+import com.launchacademy.localparksreview.repositories.VisitorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/parks")
 public class ParkRestController {
     private ParkRepository parkRepo;
+    private VisitorRepository visitorRepo;
 
 
     @Autowired
@@ -33,5 +44,26 @@ public class ParkRestController {
             throw new UrlNotFoundException();
         }
         return park;
+    }
+
+    private class InvalidVisitorException extends RuntimeException{};
+
+    @ControllerAdvice
+    private class InvalidVisitorAdvice {
+        @ResponseBody
+        @ExceptionHandler(InvalidVisitorException.class)
+        @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+        String invalidVisitor(InvalidVisitorException ic) {
+            return "";
+        }
+    }
+
+    @PostMapping
+    public Visitor newVisitor(@RequestBody Visitor visitor, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new InvalidVisitorException();
+        } else {
+            return visitorRepo.save(visitor);
+        }
     }
 }
