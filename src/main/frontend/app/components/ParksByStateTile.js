6a85524739dc91fc35626093e661a9959255950c
Lauren Sylvain <lsylvain1726@gmail.com>
2020-05-07 16:01:06 -0400

    add styles

--- src/main/frontend/app/components/ParksByStateTile.js
+++ src/main/frontend/app/components/ParksByStateTile.js
@@ -5,10 +5,17 @@ import { Link } from "react-router-dom";
 const ParksByStateTile = (props) =>{
   const path = `/parks/${props.state}/${props.id}`
     return(
-        <div>
+      <div className="small-12 medium-4 columns">
+        <div className="card-park">
+          <div className="card-park-body">
             <h3><Link to={path}>{props.name}</Link></h3>
             <p>{props.description}</p>
+          </div>
+          <div className="card-pet-footer">
+          <Link to={path} className="button button-pettype-name">Visit {props.name}</Link>
+          </div>
         </div>
+      </div>
     )
 }
 
