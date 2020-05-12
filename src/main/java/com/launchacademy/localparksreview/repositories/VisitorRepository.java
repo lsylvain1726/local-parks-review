43b2a6934149698e4e3bfedd40078673167ab0ca
Aslihan <aslihanasadova@gmail.com>
2020-05-12 12:00:39 -0400

    Visitor Controller and Form updated

--- src/main/java/com/launchacademy/localparksreview/repositories/VisitorRepository.java
+++ src/main/java/com/launchacademy/localparksreview/repositories/VisitorRepository.java
@@ -1,8 +1,12 @@
 package com.launchacademy.localparksreview.repositories;
 
 import com.launchacademy.localparksreview.models.Visitor;
+import java.util.Optional;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface VisitorRepository extends PagingAndSortingRepository<Visitor, Integer> {
 
+ Optional<Visitor> findByEmail(String email);
 }
+
+
