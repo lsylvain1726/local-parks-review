fa10e150ebb37eb7bf8f0649af875e0d7fef34eb
[Samuel Arocho] <[samuelarocho@gmail.com]>
2020-05-12 13:21:12 -0400

    edit and delete reviews update

--- src/main/frontend/app/components/ReviewShow.js
+++ src/main/frontend/app/components/ReviewShow.js
@@ -2,6 +2,17 @@ import React from "react"
 
 const ReviewShow = (props) => {
   const review = props.review
+
+  const handleEditClick = (event) => {
+    event.preventDefault()
+    props.editContractor(props.id)
+  }
+
+  const handleDeleteClick = (event) => {
+    event.preventDefault()
+    props.deleteContractor(props.id)
+  }
+
   return(
     <div>
       <p>
--- src/main/frontend/app/components/ReviewUpdateTile.js
+++ src/main/frontend/app/components/ReviewUpdateTile.js
@@ -1,17 +1,11 @@
 import React, {useState} from 'react'
 
 const ReviewUpdateTile = (props) => {
-  const handleEditClick = (event) => {
-    event.preventDefault()
-    props.editContractor(props.id)
-  }
 
-  const handleDeleteClick = (event) => {
-    event.preventDefault()
-    props.deleteContractor(props.id)
-  }
   return(
     <div>
+      <p></p>
+
     </div>
   )
 }
