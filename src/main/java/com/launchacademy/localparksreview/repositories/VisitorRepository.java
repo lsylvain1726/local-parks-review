5869232766e92b997f13c70ccc8397f4cf4d7ee9
Dan Pickett <dan.pickett@launchacademy.com>
2020-05-14 14:59:55 -0400

    add repository annotations

--- src/main/java/com/launchacademy/localparksreview/repositories/VisitorRepository.java
+++ src/main/java/com/launchacademy/localparksreview/repositories/VisitorRepository.java
@@ -3,7 +3,9 @@ package com.launchacademy.localparksreview.repositories;
 import com.launchacademy.localparksreview.models.Visitor;
 import java.util.Optional;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VisitorRepository extends PagingAndSortingRepository<Visitor, Integer> {
 
  Optional<Visitor> findByEmail(String email);
